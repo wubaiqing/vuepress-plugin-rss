3103cd2cdb2cee270e9ffaff832be4653f1e116f
wubaiqing <wubaiqing@vip.qq.com>
2019-01-02 14:45:29 +0800

    feat: 完成 1.1.0 版本

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /**
- * 新增 Markdown 转 HTML 依赖，基于 vuepree-plugin-rss 基础之上做了些改造
- *
+ * 基于 youngtailors/vuepress-plugin-rss 基础之上进行改造
+ * 
  * @see https://github.com/youngtailors/vuepress-plugin-rss/blob/master/index.js
  */
 const RSS = require('rss')
 const path = require('path')
 const fs = require('fs-extra')
-const MarkdownIt = require('markdown-it')
-const md = new MarkdownIt()
+const MarkdownIt = require('markdown-it'),
+      md = new MarkdownIt()
 
 module.exports = (pluginOptions, ctx) => {
   return {
@@ -15,7 +15,7 @@ module.exports = (pluginOptions, ctx) => {
 
     generated () {
       const { pages, sourceDir } = ctx
-      const { count = 60 } = pluginOptions
+      const { filter = () => true, count = 60 } = pluginOptions
       const siteData = require(path.resolve(sourceDir, '.vuepress/config.js'))
 
       const feed = new RSS({
@@ -28,9 +28,7 @@ module.exports = (pluginOptions, ctx) => {
       })
 
       pages
-        .filter(page => {
-          return /^\/201.+/.test(page.path)
-        })
+        .filter(page => filter(page))
         .map(page => ({...page, date: new Date(page.lastUpdated)}))
         .sort((a, b) => b.date - a.date)
         .map(page => ({
@@ -46,6 +44,8 @@ module.exports = (pluginOptions, ctx) => {
         path.resolve(ctx.outDir, 'rss.xml'),
         feed.xml()
       );
+
+      console.log(chalk.green.bold('RSS 文件已生成!'))
     }
   }
 }
